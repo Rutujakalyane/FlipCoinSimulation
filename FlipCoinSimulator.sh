fa3e409fc9f27b2542d0eb83412ab997a2ca6df7
Rutujakalyane <60542373+Rutujakalyane@users.noreply.github.com>
2020-04-05 23:17:52 +0530

    MODIFIED  :  To store percentage of doublet

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,26 +1,44 @@
 read -p "Enter the number of times coin flipped - " num
-head=0
-tail=0
+hh=0
+ht=0
+th=0
+hh=0
 for (( i=0; i<$num; i++ ))
 do
-        coin=$(( RANDOM%2))
-        if [ $coin -eq 1 ]
+        coin1=$(( RANDOM%2))
+        coin2=$(( RANDOM%2))
+
+        if [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
         then
-                ((head++))
-        else
-                ((tail++))
+                ((hh++))
+        elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
+        then
+                ((ht++))
+        elif [[ $coin1 -eq 0 && $coin2 -eq 1 ]]
+        then
+                ((th++))
+        else    
+                ((tt++))
         fi
 done
 declare -A coin
-coin[Heads]=$head
-coin[Tails]=$tail
-echo "Coin Signs-" ${!coin[@]} # All keys
-echo "Both Values-" ${coin[@]} #All values
+coin[HH]=$hh
+coin[HT]=$ht
+coin[TH]=$th
+coin[TT]=$tt
+        echo "Coin Signs-" ${!coin[@]}
+        echo "Coin Values-" ${coin[@]}
+hh1=$(($hh*100))
+hhPercent=$(( $hh1/$num ))
+ht1=$(($ht*100))
+htPercent=$(( $ht1/$num ))
+th1=$(($th*100))
+thPercent=$(( $th1/$num ))
+tt1=$(($tt*100))
+ttPercent=$(( $tt1/$num ))
 
-h=$(($head*100))
-headPercent=$(( $h/$num ))
-t=$(($tail*100))
-tailPercent=$(( $t/$num ))
+echo "HH Percentage- $hhPercent"
+echo "HT Percentage- $htPercent"
+echo "TH Percentage- $thPercent"
+echo "TT Percentage- $ttPercent"
 
-echo "Heads Percentage- $headPercent"
-echo "Tails Percentage- $tailPercent"
