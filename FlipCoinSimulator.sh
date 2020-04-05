c98b7636aa00e1e8cb6978ef041d206771f33c2a
Rutujakalyane <60542373+Rutujakalyane@users.noreply.github.com>
2020-04-05 20:25:48 +0530

    ADDED : Percentage calculation of heads/tails

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,8 +1,26 @@
-HEADS=0
-TAILS=1
-if ((RANDOM%2 == 0)
-then
-		echo "Head"
-else
-		echo "Tail"
-fi
+read -p "Enter the number of times coin flipped - " num
+head=0
+tail=0
+for (( i=0; i<$num; i++ ))
+do
+        flip=$(( RANDOM%2))
+        if [ $flip -eq 1 ]
+        then
+                ((head++))
+        else
+                ((tail++))
+        fi
+done
+declare -A coin
+coin[Heads]=$head
+coin[Tails]=$tail
+echo "Coin Signs-" ${!coin[@]} # All keys
+echo "Both Values-" ${coin[@]} #All values
+
+h=$(($head*100))
+headPercent=$(( $h/$num ))
+t=$(($tail*100))
+tailPercent=$(( $t/$num ))
+
+echo "Heads Percentage- $headPercent"
+echo "Tails Percentage- $tailPercent"
